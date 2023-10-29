eb14953190a54cf5fe4d9e994426b3b639ba65da
education-bootcamp <education.seekerscloud@gmail.com>
2023-10-29 13:35:11 +0530

    4

---
+++ src/com/devstack/pos/db/HibernateUtil.java
@@ -0,0 +1,38 @@
+package com.devstack.pos.db;
+
+import com.devstack.pos.entity.User;
+import com.devstack.pos.entity.UserRole;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+
+public class HibernateUtil {
+
+    public final static SessionFactory sessionFactory=buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory(){
+
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .addAnnotatedClass(UserRole.class)
+                .addAnnotatedClass(User.class)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        return metadata.getSessionFactoryBuilder()
+                .build();
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+}
