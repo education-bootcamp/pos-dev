e1f1eaebcee737021620e1b2d2ab83540bf691e3
education-bootcamp <education.seekerscloud@gmail.com>
2023-11-04 09:53:56 +0530

    9

--- src/com/devstack/pos/controller/LoginFormController.java
+++ src/com/devstack/pos/controller/LoginFormController.java
@@ -1,4 +1,24 @@
 package com.devstack.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane loginContext;
+
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage)loginContext.getScene().getWindow();
+        stage.setScene(
+                new Scene(
+                        FXMLLoader.load(getClass().getResource("../view/AdminPortalView.fxml"))
+                )
+        );
+        stage.centerOnScreen();
+    }
 }
